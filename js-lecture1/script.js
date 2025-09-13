dec11e40f2ca47403ee4d4562eebbb5413508ee9
Mariam Nonikashvili <marinonikashvilii@gmail.com>
2025-09-13 13:12:27 +0400

    lectures about: variables; data types

---
+++ js-lecture1/script.js
@@ -0,0 +1,61 @@
+//console.log(x)
+
+//var x=5;
+
+//console.log(x)
+
+
+let lastName = 20;
+lastName = 30;
+// const age = 29;
+// age = 49;
+//console.log(name)
+
+
+//console.log(name)
+
+//console.log(name)
+
+func()
+
+function func() {
+    //const myVariable = 1234;
+    //console.log("absolutly hoisted")
+}
+
+//console.log(myVariable)
+
+
+//primitive: string; number; boolen; bigInt; undifiend; null; sumbol
+//non-primitive: object, function, date array
+
+// if(7+9*7){
+
+//     console.log("true")
+// }else{
+// console.log("false")
+// }
+let myBigInt = 48484686468n;
+let myBigInt2 = BigInt(48484686468);
+//console.log(Number.MAX_SAFE_INTEGER)
+const numb = 4;
+
+let abc;
+//onsole.log(typeof myBigInt)
+
+let x= 10;
+let y=x;
+
+y = 20;
+console.log(x)
+
+let user = {
+    name: "mariami",
+    age:20,
+}
+
+let user2=user;
+
+user2.age=60;
+
+console.log(user)
